656b434e14c8a5d6bd1add0a05213eb6f7db139d
Mubahi <mubahilbhatti@gmail.com>
2024-03-07 19:41:18 +0500

    za-media animation

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,27 +1,31 @@
 import React from "react";
-
+import Aos from "aos";
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
 const Dashboard = ({onLogin}) => {
- 
+  useEffect(() => {
+    Aos.init({duration:2000});
+  })
     return (
       <div className="flex justify-center items-center h-screen py-40 bg-gradient-to-b from-yellow-500 to-orange-500">
         <div className="container mx-auto">
           <div className="flex flex-col lg:flex-row w-10/12 lg:w-8/12 bg-white rounded-xl mx-auto shadow-lg overflow-hidden">
             <div className="w-full lg:w-1/2 flex flex-col items-center justify-center p-12 bg-no-repeat bg-cover bg-center bg-gradient-to-b from-yellow-300 to-orange-300 rounded-ee-full">
-            <div className="">
-                  <img src="zamedia.png" className="w-68" alt="za-media-logo" />
-            </div>
+              <div className="" data-aos="fade-down">
+                    <img src="zamedia.png" className="w-68" alt="za-media-logo" />
+              </div>
             </div>
             <div className="w-full lg:w-1/2 py-16 px-12">
               <p className='text-3xl mb-6 font-bold font-serif'>Welcome</p>
               <form action="#">
                 <div className="relative mt-4">
-                    <input id="userName" type="text" placeholder="" className="text-black peer placeholder-transparent bg-yellow-100 border-l-2 border-red-500 w-full py-2 px-3 focus:outline-none inputFeild"/>
+                    <input id="userName" type="text" placeholder="" required className="text-black peer placeholder-transparent bg-yellow-100 border-l-2 border-red-500 w-full py-2 px-3 focus:outline-none inputFeild"/>
                     <label className="text-black font-mono peer-placeholder-shown:text-base peer-placeholder-shown:text-black peer-placeholder-shown:top-2 peer-focus:-top-5 peer-focus:text-black peer-focus:text-sm absolute left-2 -top-5 cursor-text transition-all" htmlFor="userName" name="userName">
                     Username...
                     </label>
                 </div>
                 <div className="relative mt-4">
-                    <input id="password" type="password" placeholder="" className="text-black peer placeholder-transparent bg-yellow-100 border-l-2 border-red-500 w-full py-2 px-3 focus:outline-none inputFeild "/>
+                    <input id="password" type="password" placeholder="" required className="text-black peer placeholder-transparent bg-yellow-100 border-l-2 border-red-500 w-full py-2 px-3 focus:outline-none inputFeild "/>
                     <label className="text-black font-mono peer-placeholder-shown:text-base peer-placeholder-shown:text-black peer-placeholder-shown:top-2 peer-focus:-top-5 peer-focus:text-black peer-focus:text-sm absolute left-2 -top-5 cursor-text transition-all " htmlFor="password" name="password">
                     Password...
                     </label>
--- src/pages/Homescreen.jsx
+++ src/pages/Homescreen.jsx
@@ -1,7 +1,12 @@
 // import ShopCard from "./components/ShopCard";
-
+import Aos from "aos";
+import 'aos/dist/aos.css'
+import React from "react";
+import { useEffect } from "react";
 const Homescreen = ({shops,Countries ,ViewShop}) => {
-   
+    useEffect(() => {
+        Aos.init({duration:2000});
+      })
     return (
         <>
             <div className="h-screen flex items-center justify-center bg-gradient-to-b from-indigo-300 via-orange-300 to-pink-300">
@@ -12,9 +17,9 @@ const Homescreen = ({shops,Countries ,ViewShop}) => {
                             );
                         })
                     } */}
-                <div class="text-center">
-                    <h1 class="text-8xl font-bold font-serif text-white ">Welcome To  
-                        <span className="text-black ml-2">
+                <div className="text-center">
+                    <h1 className="text-8xl font-bold font-serif text-white" data-aos="zoom-in">Welcome To  
+                        <span className="text-black ml-2" >
                             Za:Media
                         </span>
                     </h1>
