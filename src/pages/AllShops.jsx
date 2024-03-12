39516f367937eb197effa8dccb9deba71a7e0a23
Mubahi <mubahilbhatti@gmail.com>
2024-03-12 18:02:15 +0500

    za-media

--- src/pages/AllShops.jsx
+++ src/pages/AllShops.jsx
@@ -44,51 +44,49 @@ const AllShops = ({Shops,Brands,Categories,Areas,Countries}) => {
     return (
         <div className='pt-16 bg-gradient-to-b from-indigo-300 via-orange-300 to-pink-300'>
             <div className='border-b fixed w-full border-orange-500 flex flex-col sm:flex-row justify-around pt-1 bg-yellow-100'>
-                {/* <div className='flex flex-wrap bg-yellow-300 rounded-md py-1'> */}
-                    <select  onChange={(e) => filterCities(e.target.value)} className="text-black mx-2 w-full sm:w-28 mb-2 sm:mb-0 py-1 bg-transparent outline-none " value={SelectedCountry}>
-                        <option value="" className="bg-white text-black">All Countries</option>
+                <select  onChange={(e) => filterCities(e.target.value)} className="text-black mx-2 w-full sm:w-28 mb-2 sm:mb-0 py-1 bg-transparent outline-none " value={SelectedCountry}>
+                    <option value="" className="bg-white text-black">All Countries</option>
                         {Countries.map(function(country){
-                            return(<option key={country.country_id} value={country.country_id} className="bg-white text-black">{country.country_name}</option>);
-                        })}
-                    </select>
-                    <select  onChange={(e) => setSelectedCity(e.target.value)} value={SelectedCity} className="text-black mx-2 w-full sm:w-28 mb-2 sm:mb-0 py-1 bg-transparent outline-none">
-                        <option value="all" className="bg-white text-black">All Cities </option>
+                         return(<option key={country.country_id} value={country.country_id} className="bg-white text-black">{country.country_name}</option>);
+                    })}
+                </select>
+                <select  onChange={(e) => setSelectedCity(e.target.value)} value={SelectedCity} className="text-black mx-2 w-full sm:w-28 mb-2 sm:mb-0 py-1 bg-transparent outline-none">
+                    <option value="all" className="bg-white text-black">All Cities </option>
                         {Cities.map(function(City){
-                            return(<option key={City.city_id} value={City.city_id} className="bg-white text-black">{City.city_name}</option>);
-                        })}
-                    </select>
-                    <select  className="text-black mx-2 w-full sm:w-28 mb-2 sm:mb-0 py-1 bg-transparent outline-none">
-                        <option value="all" className="bg-white text-black">All Brands</option>
+                        return(<option key={City.city_id} value={City.city_id} className="bg-white text-black">{City.city_name}</option>);
+                    })}
+                </select>
+                <select  className="text-black mx-2 w-full sm:w-28 mb-2 sm:mb-0 py-1 bg-transparent outline-none">
+                    <option value="all" className="bg-white text-black">All Brands</option>
                         {Brands.map(function(brand){
-                            return(<option key={brand.id} value={brand.id} className="bg-white text-black">{brand.name}</option>);
-                        })}
-                    </select>
-                    <select  className="text-black mx-2 w-full sm:w-28 mb-2 sm:mb-0 py-1 bg-transparent outline-none">
-                        <option value="all" className="bg-white text-black">Categories</option>
+                        return(<option key={brand.id} value={brand.id} className="bg-white text-black">{brand.name}</option>);
+                    })}
+                </select>
+                <select  className="text-black mx-2 w-full sm:w-28 mb-2 sm:mb-0 py-1 bg-transparent outline-none">
+                    <option value="all" className="bg-white text-black">Categories</option>
                         {Categories.map(function(Category){
-                            return(<option key={Category.id} value={Category.id} className="bg-white text-black">{Category.name}</option>);
-                        })}
-                    </select>
-                    <select  value={SelectedArea}  onChange={(e) => setSelectedArea(e.target.value)} className="text-black mx-2 w-full sm:w-28 mb-2 sm:mb-0 py-1 bg-transparent outline-none">
-                        <option value="all" className="bg-white text-black">All Areas</option>
+                        return(<option key={Category.id} value={Category.id} className="bg-white text-black">{Category.name}</option>);
+                    })}
+                </select>
+                <select  value={SelectedArea}  onChange={(e) => setSelectedArea(e.target.value)} className="text-black mx-2 w-full sm:w-28 mb-2 sm:mb-0 py-1 bg-transparent outline-none">
+                    <option value="all" className="bg-white text-black">All Areas</option>
                         {Areas.map(function(area){
-                            return(<option key={area.id} value={area.id} className="bg-white text-black">{area.name}</option>);
-                        })}
-                    </select>
-                    <select  value={SelectedWindows} onChange={(e) => setSelectedWindows(e.target.value)} className="text-black mx-2 w-full sm:w-28 mb-2 sm:mb-0 py-1 bg-transparent outline-none">
-                        <option value="all" className="bg-white text-black">Window</option>
-                        <option value="1" className="bg-white text-black">1</option>
-                        <option value="2" className="bg-white text-black">2 </option>
-                        <option value="3" className="bg-white text-black">3 </option>
-                        <option value="4" className="bg-white text-black">4 </option>
-                        <option value="5" className="bg-white text-black">5 </option>
-                        <option value="6" className="bg-white text-black">6 </option>
-                        <option value="7" className="bg-white text-black">7 </option>
-                        <option value="8" className="bg-white text-black">8 </option>
-                        <option value="9" className="bg-white text-black">9 </option>
-                        <option value="10" className="bg-white text-black">10 </option>
-                    </select>
-                {/* </div> */}
+                        return(<option key={area.id} value={area.id} className="bg-white text-black">{area.name}</option>);
+                    })}
+                </select>
+                <select value={SelectedWindows} onChange={(e) => setSelectedWindows(e.target.value)} className="text-black mx-2 w-full sm:w-28 mb-2 sm:mb-0 py-1 bg-transparent outline-none">
+                    <option value="all" className="bg-white text-black">Window</option>
+                    <option value="1" className="bg-white text-black">1</option>
+                    <option value="2" className="bg-white text-black">2 </option>
+                    <option value="3" className="bg-white text-black">3 </option>
+                    <option value="4" className="bg-white text-black">4 </option>
+                    <option value="5" className="bg-white text-black">5 </option>
+                    <option value="6" className="bg-white text-black">6 </option>
+                    <option value="7" className="bg-white text-black">7 </option>
+                    <option value="8" className="bg-white text-black">8 </option>
+                    <option value="9" className="bg-white text-black">9 </option>
+                    <option value="10" className="bg-white text-black">10 </option>
+                </select>
             </div>
             <div className="overflow-x-auto shadow-md min-h-screen pt-8">
                 <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
