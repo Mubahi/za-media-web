172730572e8191fe6604c5fef7b38a058e08806b
IshaaBari <isha.bari579@gmail.com>
2024-04-02 13:36:47 +0500

    change in event component

--- src/pages/Events/Event.jsx
+++ src/pages/Events/Event.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import PageHeading from "../../components/PageHeadng";
 import Divider from "../../components/Divider";
 import FormField from "../../components/FormField";
 import Date from "../../components/Date";
 const Event = () => {
+  const [fieldData, setFieldData] = useState({});
+  const DisplayData = (e) => {
+    setFieldData({ ...fieldData, [e.target.name]: e.target.value });
+  };
   return (
     <>
       <PageHeading Title="Basic info" />
       <Divider />
-      <FormField name="event_address" placeholder="Event address" type="text" />
+      <FormField
+        name="event_address"
+        placeholder="Event address"
+        type="text"
+        onChange={DisplayData}
+      />
       {/* <FormField name="event_date" placeholder="mm/dd/yyyy" type="date" /> */}
       <Date />
       <FormField
@@ -16,18 +25,21 @@ const Event = () => {
         placeholder="Event days"
         type="number"
         min="1"
+        onChange={DisplayData}
       />
       <FormField
         name="event_starting_hour"
         placeholder="Event starting hour"
         type="number"
         min="0"
+        onChange={DisplayData}
       />
       <FormField
         name="event_closing_hour"
         placeholder="Event closing hour"
         type="number"
         min="0"
+        onChange={DisplayData}
       />
     </>
   );
